63b77f5d0f0b1aa0ef8e50449a0713bcc124bc71
Hristina <malkata_hrisi@abv.bg>
2017-05-30 01:05:31 +0300

    change XPath for ErrorMessageForInvalidLoginData
    it has more of one "/"

--- Blog.Unit.Tests/Pages/LoginPage/LoginPageMap.cs
+++ Blog.Unit.Tests/Pages/LoginPage/LoginPageMap.cs
@@ -74,11 +74,10 @@ namespace UnitTests.Pages.LoginPage
         {
             get
             {   
-                var xpath = "//html/body/div[2]/div/div/form/div[1]/ul/li";
+                var xpath = "/html/body/div[2]/div/div/form/div[1]/ul/li";
                 this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
                 return this.Driver.FindElement(By.XPath(xpath));
             }
-        }
-        
+        }       
     }
 }
