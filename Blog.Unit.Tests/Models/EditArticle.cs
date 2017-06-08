a6bb57b7d84bf5907f274186829924070363dd1c
Hristina <malkata_hrisi@abv.bg>
2017-06-08 12:41:29 +0300

    Add new test
    Create new EditArticle class and add CheckArticleIsEdit test

---
+++ Blog.Unit.Tests/Models/EditArticle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Unit.Tests.Models
+{
+    public class EditArticle
+    {
+        private string title;
+        private string content;
+
+        public EditArticle(string title, string content)
+        {
+            this.title = title;
+            this.content = content;
+        }
+
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
+
+        public string Content
+        {
+            get { return this.content; }
+            set { this.content = value; }
+        }
+    }
+}
